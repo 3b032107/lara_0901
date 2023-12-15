41c95a946e4de0b88abed9c278514f62e195f80b
廖家禾 <3b032107@gm.student.ncut.edu.tw>
2023-12-15 13:43:15 +0800

    作業第一次檢查未OK，已做修正

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,53 +3,33 @@
 @section('page-title', 'Create article')
 
 @section('page-content')
-
-<div class="container-fluid px-4">
-    <h1 class="mt-4">文章管理</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">新增文章</li>
-    </ol>
-    <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
-        <strong>錯誤！</strong> 請修正以下問題：
-        <ul>
-            <li>錯誤 1</li>
-        </ul>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        @if (count($errors) > 0)
-            <!-- 表單錯誤清單 -->
-                <strong>哎呀! 出了些問題!</strong>
-
-                <br><br>
-        @include('layouts.shared.errors')
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        @endif
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">文章管理</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">新增文章</li>
+        </ol>
+        @include('admin.layouts.shared.errors')
+        <form action="{{route('admin.posts.store')}}" method="POST">
+            @method('POST')
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">標題：</label>
+                <input id="title" name="title" type="text" class="form-control" placeholder="請輸入文章標題">
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">內容：</label>
+                <textarea id="content" name="content" class="form-control" rows="10" placeholder="請輸入文章內容"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="is_feature" class="form-label">精選？</label>
+                <select id="is_feature" name="is_feature">
+                    <option value="0">否</option>
+                    <option value="1">是</option>
+                </select>
+            </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button class="btn btn-primary btn-sm" type="submit">儲存</button>
+            </div>
+        </form>
     </div>
-    <form action="{{ route('admin.posts.store') }}" method="POST" role="form">
-        @method('POST')
-        @csrf
-        <div class="form-group">
-            <label for="title" class="form-label">文章標題:</label>
-            <input id="title" name="title" type="text" class="form-control" value="{{old('title')}}" placeholder="請輸入文章標題">
-        </div>
-        <div class="form-group">
-            <label for="content" class="form-label">文章內容：</label>
-            <textarea id="content" name="content" class="form-control" rows="10">{{old('content')}}</textarea>
-        </div>
-        <div class="form-group">
-            <label for=" is_feature " class="form-label">精選？</label>
-            <select id=" is_feature " name="is_feature" class="form-control">
-                <option value="0">否</option>
-                <option value="1">是</option>
-            </select>
-        </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-primary btn-sm" type="submit">儲存</button>
-        </div>
-    </form>
-</div>
 @endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,64 +3,44 @@
 @section('page-title', 'Article list')
 
 @section('page-content')
-<div class="container-fluid px-4">
-    <h1 class="mt-4">文章管理</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">文章一覽表</li>
-    </ol>
-    <div class="alert alert-success alert-dismissible" role="alert" id="liveAlert">
-        <strong>完成！</strong> 成功儲存文章
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-        <a class="btn btn-success btn-sm" href="{{ route('admin.posts.create') }}">新增</a>
-    </div>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col" style="text-align: left">標題</th>
-            <th scope="col" style="text-align: right">精選?</th>
-            <th scope="col">功能</th>
-        </tr>
-        </thead>
-        @foreach($posts  as $post)
-        <tbody>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">文章管理</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">文章一覽表</li>
+        </ol>
+        <div class="alert alert-success alert-dismissible" role="alert" id="liveAlert">
+            <strong>完成！</strong> 成功儲存文章
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a class="btn btn-success btn-sm" href="{{ route('admin.posts.create') }}">新增</a>
+        </div>
+        <table class="table">
+            <thead>
             <tr>
-                <th scope="row" style="width: 50px">{{ $post->id }}</th>
-                <td style="text-align: right">{{ $post->id }}</td>
-                <td>{{ $post->title }}</td>
-                <td style="text-align: right">{{ ($post->is_feature)? 'v':'x' }}</td>
-                <td style="width: 150px">
-                    <a class="btn btn-sm btn-primary" href="{{ route('admin.posts.edit',$post->id) }}">編輯</a>
-                    <form action="{{ route('admin.posts.update',$post->id) }}" method="POST" role="form">
-                        @method('PATCH')
-                        @csrf
-                        <div class="form-group">
-                            <label for="title" class="form-label">標籤：</label>
-                            <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{ old('title',$post->title) }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="content" class="form-label">內容：</label>
-                            <textarea id="content" name="content" class="form-control" rows="10"> {{ old('content',
-$post->content) }}</textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="is_feature " class="form-label">精選？</label>
-                            <select id="is_feature " name="is_feature" class="form-control" >
-                                <option value="0" {{ (!$post->is_feature)? ' selected' : '' }}>否</option>
-                                <option value="1" {{ ($post->is_feature)? ' selected' : '' }}>是</option>
-                            </select>
-                    </form>
-                    <form action="{{ route('admin.posts.destroy',$post->id) }}" method="POST" style="display: inline-block">
-                        @method('DELETE')
-                        @csrf
-                        <button class="btn btn-sm btn-danger" type="submit">刪除</button>
-                    </form>
-                </td>
+                <th scope="col">#</th>
+                <th scope="col" style="text-align:left;">標題</th>
+                <th scope="col" style="text-align:right;">精選？</th>
+                <th scope="col">功能</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td style="text-align:right;">{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td style="text-align:right;">{{ ($post->is_feature)?'v':'x' }}</td>
+                    <td>
+                        <a href="{{route('admin.posts.edit', $post->id)}}" type="button" class="btn btn-primary btn-sm">編輯</a>
+                        <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" style="display:inline-block;">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-danger btn-sm">刪除</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
